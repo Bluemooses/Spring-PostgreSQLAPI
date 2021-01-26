03bbab2da6ff45d14f551fde8043d62928f3b90f
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-01-26 11:56:38 -0600

    Added a basic rest controller

--- src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
+++ src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
@@ -2,6 +2,10 @@ package com.example.postgresdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class PostgresDemoApplication {
@@ -9,5 +13,12 @@ public class PostgresDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(PostgresDemoApplication.class, args);
     }
+
+    @GetMapping("/get")
+    public @ResponseBody
+    ResponseEntity<String> get() {
+        return new ResponseEntity<String>("GET Response", HttpStatus.OK);
+    }
+
 
 }
---
+++ src/main/java/com/example/postgresdemo/RESTController.java
@@ -0,0 +1,34 @@
+package com.example.postgresdemo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class RESTController {
+
+    @GetMapping("/get")
+    public @ResponseBody
+    ResponseEntity<String> get() {
+        return new ResponseEntity<String>("GET Response", HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public @ResponseBody ResponseEntity<String> post() {
+        return new ResponseEntity<String>("POST Response", HttpStatus.OK);
+    }
+
+    @PutMapping("/put")
+    public @ResponseBody ResponseEntity<String> put() {
+        return new ResponseEntity<String>("PUT Response", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete")
+    public @ResponseBody ResponseEntity<String> delete() {
+        return new ResponseEntity<String>("DELETE Response", HttpStatus.OK);
+    }
+
+    @PatchMapping("/patch")
+    public @ResponseBody ResponseEntity<String> patch() {
+        return new ResponseEntity<String>("PATCH Response", HttpStatus.OK);
+    }
+}
